70330fbd893e7c3e4c98f911a4fbebc0cb75ee2a
akhiltmohan <braveheartcreation@gmail.com>
2021-02-06 18:59:16 +0530

    added test cases

--- app/src/main/java/com/braveheartcreations/myapplication/models/ListItem.kt
+++ app/src/main/java/com/braveheartcreations/myapplication/models/ListItem.kt
@@ -5,12 +5,12 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class ListItem(
-    val copyright: String = "",
-    val date: String = "",
-    val explanation: String = "",
-    val hdurl: String = "",
-    val media_type: String = "",
-    val service_version: String = "",
-    val title: String = "",
-    val url: String = ""
+    val copyright: String,
+    val date: String,
+    val explanation: String,
+    val hdurl: String,
+    val media_type: String,
+    val service_version: String,
+    val title: String,
+    val url: String
 ) : Parcelable
